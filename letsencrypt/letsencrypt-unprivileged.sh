89e1b2b3355cfeadee33bba47fa829038d52707f
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-02-26 14:16:29 +0100

    Fix a small bug & let it re-run every week

--- letsencrypt/letsencrypt-unprivileged.sh
+++ letsencrypt/letsencrypt-unprivileged.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 lastmod() {
+  [[ -z "$1" || -z "$1" == *'*'* ]] && return
   echo $(( $(date +%s) - $(date +%s -r "$1") ))
 }
 
-# one month
-declare interval=$((30*24*60*60)) 
-declare lastRun=$(lastmod /etc/letsencrypt/live/*/cert.pem | sed -n 1p)
+while true
+do
+  # one month
+  declare interval=$((30*24*60*60)) 
+  declare lastRun=$(lastmod /etc/letsencrypt/live/*/cert.pem | sed -n 1p)
 
-if [[ -z $lastRun || $(($lastRun > $interval)) -gt 0 ]]; then
-  declare IDS="$1"
-  shift
-  sleep 10
-  letsencrypt "$@"
-  if [[ $IDS != root ]]; then
-    chown -R $IDS /etc/letsencrypt
+  if [[ -z $lastRun || $(($lastRun > $interval)) -gt 0 ]]; then
+    declare IDS="$1"
+    shift
+    sleep 10
+    letsencrypt "$@"
+    if [[ $IDS != root ]]; then
+      chown -R $IDS /etc/letsencrypt
+    fi
+  else
+    echo "Skipping LetsEncrypt since the certificate should still be valid"
   fi
-else
-  echo "Skipping LetsEncrypt since the certificate should still be valid"
-fi
 
-# TODO
-# letsencrypt  | date: /etc/letsencrypt/live/*/cert.pem: No such file or directory
-# letsencrypt  | /opt/letsencrypt/letsencrypt-unprivileged.sh: line 4: 1456416859 -  : syntax error: operand expected (error token is "-
-#  ")
+  sleep $(( 7 * 24 * 60 * 60 ))
+done
