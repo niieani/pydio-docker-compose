61006113f6495ce8485215e3fc1d3551a8f843fb
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2017-05-13 21:31:09 +0200

    feat(all): update to v2
    use dehydrated for letsencrypt
    use .env for defining docker-compose vars
    use pure nginx image
    move pydio files out of the image

---
+++ getPydio.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# get Pydio source
+PYDIO_VERSION=7.0.4
+
+wget https://download.pydio.com/pub/core/archives/pydio-core-${PYDIO_VERSION}.tar.gz && \
+tar -zxf pydio-core-${PYDIO_VERSION}.tar.gz
+
+mkdir -p data
+mv pydio-core-${PYDIO_VERSION} data/pydio && \
+chown -R www-data:www-data data/pydio
+rm pydio-core-${PYDIO_VERSION}.tar.gz
+
+# get initial certs before first run
