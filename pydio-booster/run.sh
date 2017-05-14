03c94461b9c44613a42dd178924180b3d1ff51e5
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2017-05-14 12:02:18 +0200

    feat(booster): add pydio-booster

---
+++ pydio-booster/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+APIKEY=$(< /srv/www/data/plugins/core.mq/apikey)
+export TOKENP=${APIKEY##*:}
+export TOKENS=${APIKEY%%:*}
+
+envsubst < pydiocaddy.template > pydiocaddy
+envsubst < pydioconf.template > pydioconf
+
+exec pydio-booster -c pydioconf
